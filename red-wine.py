fe6457815b1f898abdbf60ab995654d9733c4919
GINASIDHU <gina.sidhu@capgemini.com>
2023-03-10 11:08:27 +0000

    Data for red added and plot
    Signed-off-by: GINASIDHU <gina.sidhu@capgemini.com>

--- red-wine.py
+++ red-wine.py
@@ -1,1 +1,8 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+red = pd.read_csv(r'C:\Users\gisidhu\Documents\gurgina SIDHU\Data Science Material\DataSets\Red.csv')
+red.head(5)
+type(red)
+plt.plot(red.Price, red.Year)
+plt.show()
+
